61e5ee28258b8f1e08e048c3b61e0da5a2464254
dabramovitch <87294351+dabramovitch@users.noreply.github.com>
2022-03-11 16:17:51 -0800

    Add files via upload

---
+++ dmft_calc.py
@@ -0,0 +1,76 @@
+import numpy as np
+from math import pi
+from triqs.gf import *
+import triqs.gf.tools as tools
+from triqs.plot.mpl_interface import oplot,plt
+from triqs import operators as tops
+import triqs_cthyb
+from h5 import *
+import time
+
+class DmftCalc():
+    def __init__(self,beta, gf_struct, dispersions, h_int,mu, ks,solver = triqs_cthyb.Solver,defaultSolverInputs = {'n_cycles' : 20000, 'length_cycle' : 100, 'n_warmup_cycles' : 4000},verbosity = "high"):
+        self.beta = beta
+        self.gf_struct = gf_struct
+        self.dispersions = dispersions
+        self.h_int = h_int
+        self.mu = mu
+        self.mudensities = {mu:[]}
+        self.ks = ks
+        self.Nk = len(self.ks)
+        self.S = solver(beta = self.beta, gf_struct = self.gf_struct)
+        self.S.Sigma_iw << mu
+        self.Gks = {k : self.S.G0_iw.copy() for k in ks}
+        # ^ arguably, this should not be stored since it can be calculated from much less information in some cases
+        self.timeSC,self.timeImp = 0.0,0.0
+        self.defaultSolverInputs = defaultSolverInputs
+        self.verbosity = verbosity
+        self.updateGFs()
+
+    def calcGk_iw(self, Sigmak = lambda k : 0 ):
+        for k in self.ks:
+            sigmak = self.S.Sigma_iw.copy()
+            sigmak << sigmak + Sigmak(k)
+            self.Gks[k].zero()
+            for name,indices in self.gf_struct:
+                for i in indices:
+                    self.Gks[k][name][i,i] << inverse(iOmega_n - self.dispersions[(name,i)](k) + self.mu - sigmak[name][i,i])
+
+    def calcGimp_iw(self):
+        self.S.G0_iw.zero()
+        Gimp = self.S.G0_iw.copy()
+        for k in self.ks:
+            Gimp << Gimp + self.Gks[k]
+        Gimp << Gimp / self.Nk
+        self.S.G0_iw << tools.dyson(G_iw = Gimp, Sigma_iw = self.S.Sigma_iw)
+
+    def updateGFs(self,Sigmak = lambda k : 0):
+        self.calcGk_iw(Sigmak = Sigmak)
+        self.calcGimp_iw()
+
+    def solveImpurity(self,**kwargs):
+        if kwargs == {}:
+            kwargs = self.defaultSolverInputs
+        self.S.solve(h_int = self.h_int,**kwargs)
+
+    def runDmftLoop(self,nloops = 1,Sigmak = lambda k : 0,**solverkwargs):
+        # ToDo: add ramp up capability for ncycles 
+        for i in range(nloops):
+            tstart = time.time()
+            self.solveImpurity(**solverkwargs)
+            self.timeImp += time.time() - tstart
+            tstart = time.time()
+            self.updateGFs(Sigmak = Sigmak)
+            self.timeSC += time.time() - tstart
+            self.mudensities[self.mu].append(self.totalDensity())
+
+    def totalDensity(self):
+        density = self.S.G_iw.density()
+        return sum([np.trace(density[name]).real for name, indices in self.gf_struct])
+
+    def updatemu(self,mu,Sigmak = lambda k : 0):
+        # resets solver with default self energy (= mu) and corresponding G0_imp
+        self.mu = mu
+        self.S.Sigma_iw << self.mu
+        self.updateGFs(Sigmak = Sigmak)
+        self.mudensities[self.mu] = []
---
+++ testDmftCalc.py
@@ -0,0 +1,61 @@
+from dmft_calc import *
+
+beta = 10
+t = 1
+U = 4
+Nk = 400
+mu = U/2
+ks = np.linspace(-np.pi,np.pi,Nk,endpoint = False)
+gf_struct = [('up', [0]), ('down', [0])]
+epsk = lambda k:-2*t*np.cos(k)
+dispersions = {('up',0) : epsk, ('down',0) : epsk}
+h_int = U*tops.n('up',0)*tops.n('down',0)
+nloops = 2
+C10 = DmftCalc(beta,gf_struct,dispersions,h_int,mu,ks,defaultSolverInputs = {'n_cycles' : 10000, 'length_cycle' : 100, 'n_warmup_cycles' : 2000})
+C10.runDmftLoop(nloops = nloops)
+C20 = DmftCalc(beta,gf_struct,dispersions,h_int,mu,ks,defaultSolverInputs = {'n_cycles' : 20000, 'length_cycle' : 100, 'n_warmup_cycles' : 4000})
+C20.runDmftLoop(nloops = nloops)
+C40 = DmftCalc(beta,gf_struct,dispersions,h_int,mu,ks,defaultSolverInputs = {'n_cycles' : 10*40000, 'length_cycle' : 100, 'n_warmup_cycles' : 10*8000})
+C40.runDmftLoop(nloops = 10)
+oplot(C40.S.G_iw['up'],label = "hubbard")
+#C80 = DmftCalc(beta,gf_struct,dispersions,h_int,mu,ks,defaultSolverInputs = {'n_cycles' : 80000, 'length_cycle' : 100, 'n_warmup_cycles' : 16000})
+#C80.runDmftLoop(nloops = 20)
+#C160 = DmftCalc(beta,gf_struct,dispersions,h_int,mu,ks,defaultSolverInputs = {'n_cycles' : 160000, 'length_cycle' : 100, 'n_warmup_cycles' : 32000})
+#C160.runDmftLoop(nloops = 20)
+
+"""plt.plot(C10.mudensities[mu],label = "10k cycles")
+plt.plot(C20.mudensities[mu],label = "20k cycles")
+plt.plot(C40.mudensities[mu],label = "40k cycles")
+#plt.plot(C80.mudensities[mu],label = "80k cycles")
+#plt.plot(C160.mudensities[mu],label = "160k cycles")
+plt.xlabel("dmft loop iteration")
+plt.ylabel("calculated density")
+plt.title("Density Convergence (U = 4, $\mu$ = U/2)")
+plt.legend()
+plt.show()"""
+
+# example with Sigma eph
+omega = 0.2
+g = 0.1
+Sigma_ephk = {k:C40.S.Sigma_iw.copy() for k in ks}
+
+k0 = ks[0]
+Sigma_ephk[k0].zero()
+for q in ks:
+    kpq = ((k0 + q) + np.pi) % (2*np.pi) - np.pi
+    epskq = dispersions[('up',0)](kpq)
+    Nq = 1 / (np.exp(omega*beta) - 1)
+    fkpq = 1 / (np.exp(epskq*beta) + 1)
+    Sigma_ephk[k0]['up'][0,0] << Sigma_ephk[k0]['up'][0,0] + (g**2)*((Nq + fkpq)*inverse(iOmega_n - epskq + omega) - (Nq + 1 - fkpq)*inverse(iOmega_n - epskq - omega))
+    #Sigma_ephk[k]['down'][0,0] << Sigma_ephk[k]['down'][0,0] + (g**2)*((Nq + fkpq)*inverse(iOmega_n - epskq + omega) - (Nq + 1 - fkpq)*inverse(iOmega_n - epskq - omega))
+Sigma_ephk[k0]['down'][0,0] << Sigma_ephk[k0]['up'][0,0]
+
+for k in ks[1:]:
+    Sigma_ephk[k].zero()
+    Sigma_ephk[k] << Sigma_ephk[k0]
+    # works since they are k independent
+
+for mufact in [1.0,1.2,1.4,1.5,1.6]:
+    C40.updatemu(U/2 * mufact,Sigmak = lambda k : Sigma_ephk[k])
+    C40.runDmftLoop(nloops = 10, Sigmak = lambda k: Sigma_ephk[k])
+    oplot(C40.S.G_iw['up'],label = "hubbard holstein")
